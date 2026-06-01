038ccab1afeceb54cef21fba312ad59ef3f2a5ac
lu <qq844303749@gmail.com>
2026-05-31 21:29:42 -0600

    2026.05.31

---
+++ maize_dataset.py
@@ -0,0 +1,47 @@
+import pyreadr
+from pyfaidx import Fasta
+
+if __name__ == '__main__':
+    ref = Fasta(r'ref/Zea_mays.AGPv2.dna.toplevel.fa')
+    ref
+    def read_robj(fp):
+        robj, rp = pyreadr.read_r(fp), fp
+        objs = pyreadr.list_objects(rp)[0]['object_name']
+        robj = robj[objs]
+        return robj
+
+    bian_map = read_robj(r'Bian/Map.RData')
+
+    def marker_read():
+        map_filtered = bian_map
+        map_filtered = {y['number']:y for y in map(lambda x:x.to_dict(),map_filtered.iloc )}
+        return map_filtered
+        pass
+
+    def hapmap_read():
+         return [ read_robj(r'Bian/Parents26_chr'+str(num)+'.RData') for num in range(1,11)]
+
+    hapmap_list = hapmap_read()
+
+    marker_map = marker_read()
+
+    def marker_coverage(name_marker):
+        prev_marker = int(name_marker[1:]) - 1
+        frm = 0
+        if not prev_marker == 0:
+            frm  = int(marker_map['m'+str(prev_marker)]['agp_pos'])
+        to = int(marker_map[name_marker]['agp_pos'])
+        return marker_map[name_marker]['chr'],frm,to
+
+    marker_coverage('m17')
+
+    def readNAMPHT():
+        fp = r'Bian/NAMPHT.RData'
+        robj = read_robj(fp)
+        for row in robj.itertuples(index=False):
+            row_dict = row._asdict()
+            for k, v in row_dict.items():
+                pass
+
+
+    readNAMPHT()
